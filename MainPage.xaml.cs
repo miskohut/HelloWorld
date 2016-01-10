38b4c9a7bbb097c18863d1fc895a086380e1d620
Michal Kohút <miskohut@gmail.com>
2016-01-10 14:47:47 +0100

    final commit
    final commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        //handle Display button press
+        private async void buttonDisplay_Click(object sender, RoutedEventArgs e) {
+
+            //access TextBox according to the Name attribute defined in MainPage.xaml
+            String messageText = "Hello " + nameBox.Text + "!"; 
+
+            var messageDialog = new MessageDialog(messageText);
+            await messageDialog.ShowAsync();
+        }
+    }
+}
